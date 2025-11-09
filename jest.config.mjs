f54b227507dbad4d8a3669296e5f79724a549b67
nandul2424 <nandulhissella@gmail.com>
2025-11-09 17:08:38 +0530

    adding configuration of jest

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,5 +2,7 @@ export default {
     testEnvironment: "node",
     setupFilesAfterEnv: ["<rootDir>/jest.setup.mjs"],
     transform: {}, // disable Babel transform since we use native ESM
-    extensionsToTreatAsEsm: [".js"], // make Jest treat .js files as ESM
+    extensionsToTreatAsEsm: [".js"],
+
+    // make Jest treat .js files as ESM
 };
